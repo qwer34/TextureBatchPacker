a9fafe8d3b80e301ee2f82549e36366d6516a710
Xin Zhang <qwer34@gmail.com>
2015-09-01 22:10:57 +0800

    增加命令行参数分析逻辑。启动时检测TP是否安装。

--- Program.cs
+++ Program.cs
@@ -3,13 +3,95 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PowerArgs;
+using PowerArgs.Cli;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
 
 namespace TextureBatchPacker
 {
+	class TextureBatchPackerArgs
+	{
+		[HelpHook, ArgShortcut("?"), ArgDescription("Shows this help.")]
+		public bool Help { get; set; }
+
+		[HelpHook, ArgShortcut("--help"), ArgDescription("Shows this help.")]
+		public bool ShowHelp { get; set; }
+
+		[HelpHook, ArgShortcut("--version"), ArgDescription("(--version) Print the version info.")]
+		public bool ShowVersion { get; set; }
+
+		[ArgRequired(PromptIfMissing = false), ArgExistingDirectory, ArgShortcut("i"), ArgDescription("The input directory.")]
+		public string InputDirectory { get; set; }
+
+		[ArgRequired(PromptIfMissing = false), ArgExistingDirectory, ArgShortcut("o"), ArgDescription("The output directory.")]
+		public string OutputDirectory { get; set; }
+
+		public void Main()
+		{
+			Console.WriteLine("InputDirectory: {0}", InputDirectory);
+			Console.WriteLine("OutputDirectory: {0}", OutputDirectory);
+		}
+	}
+
 	class Program
 	{
-		static void Main(string[] args)
+		static int Main(string[] args)
 		{
+			Console.WriteLine("{0} Ver. {1}", Application.ProductName, Application.ProductVersion);
+			Console.WriteLine("Powered by Xin Zhang");
+			Console.WriteLine("{0}\r\n", System.IO.File.GetLastWriteTime(Application.ExecutablePath));
+
+			try
+			{
+				Process processTP = new Process();
+
+				//获得系统信息，使用的是 systeminfo.exe 这个控制台程序
+				processTP.StartInfo.FileName = "TexturePacker.exe";
+				processTP.StartInfo.Arguments = "--version";
+
+				//将cmd的标准输入和输出全部重定向到.NET的程序里
+				processTP.StartInfo.UseShellExecute = false; //此处必须为false否则引发异常
+
+				//processPngQuant.StartInfo.RedirectStandardInput = true; //标准输入
+				processTP.StartInfo.RedirectStandardOutput = true; //标准输出
+
+				//不显示命令行窗口界面
+				processTP.StartInfo.CreateNoWindow = true;
+				processTP.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+				//Console.WriteLine("打包 {0}", NameWithoutExt);
+				processTP.Start(); //启动进程
+				Console.WriteLine(processTP.StandardOutput.ReadToEnd());
+				processTP.WaitForExit();
+				processTP.Dispose();
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+				Console.WriteLine("TexturePacker not installed.");
+#if DEBUG
+				Console.ReadKey();
+#endif
+				return -1;
+			}
+
+			try
+			{
+				string[] newArgs = { "-i", "C:\\", "-o", "D:\\" };
+				Args.InvokeMain<TextureBatchPackerArgs>(newArgs);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+
+#if DEBUG
+			Console.ReadKey();
+#endif
+
+			return 0;
 		}
 	}
 }
