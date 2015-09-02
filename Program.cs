8f9e7994e8853dde088e8698eb35cf57b2d1d3a0
Xin Zhang <qwer34@gmail.com>
2015-09-02 18:12:29 +0800

    增加三个参数。

--- Program.cs
+++ Program.cs
@@ -22,14 +22,26 @@ namespace TextureBatchPacker
 		[HelpHook, ArgShortcut("--version"), ArgDescription("(--version) Print the version info.")]
 		public bool ShowVersion { get; set; }
 
-		[ArgRequired(PromptIfMissing = false), ArgExistingDirectory, ArgShortcut("i"), ArgDescription("The input directory.")]
+		[ArgDefaultValue("Editor"), ArgDescription("Packing mode - Editor, iOS, Android.")]
+		public string Mode { get; set; }
+
+		[ArgDefaultValue(0.5), ArgDescription("Scale factor.")]
+		public float Scale { get; set; }
+
+		[ArgDefaultValue(false), ArgDescription("Removes image file extensions from the sprite names - e.g. .png, .tga.")]
+		public bool TrimSpriteNames { get; set; }
+
+		[ArgRequired(PromptIfMissing = false), ArgExistingDirectory, ArgDescription("The input directory.")]
 		public string InputDirectory { get; set; }
 
-		[ArgRequired(PromptIfMissing = false), ArgExistingDirectory, ArgShortcut("o"), ArgDescription("The output directory.")]
+		[ArgRequired(PromptIfMissing = false), ArgExistingDirectory, ArgDescription("The output directory.")]
 		public string OutputDirectory { get; set; }
 
 		public void Main()
 		{
+			Console.WriteLine("Mode: {0}", Mode);
+			Console.WriteLine("Scale: {0}", Scale);
+			Console.WriteLine("TrimSpriteNames: {0}", TrimSpriteNames);
 			Console.WriteLine("InputDirectory: {0}", InputDirectory);
 			Console.WriteLine("OutputDirectory: {0}", OutputDirectory);
 		}
@@ -47,21 +59,19 @@ namespace TextureBatchPacker
 			{
 				Process processTP = new Process();
 
-				//获得系统信息，使用的是 systeminfo.exe 这个控制台程序
 				processTP.StartInfo.FileName = "TexturePacker.exe";
 				processTP.StartInfo.Arguments = "--version";
 
 				//将cmd的标准输入和输出全部重定向到.NET的程序里
 				processTP.StartInfo.UseShellExecute = false; //此处必须为false否则引发异常
 
-				//processPngQuant.StartInfo.RedirectStandardInput = true; //标准输入
+				//processTP.StartInfo.RedirectStandardInput = true; //标准输入
 				processTP.StartInfo.RedirectStandardOutput = true; //标准输出
 
 				//不显示命令行窗口界面
 				processTP.StartInfo.CreateNoWindow = true;
 				processTP.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
 
-				//Console.WriteLine("打包 {0}", NameWithoutExt);
 				processTP.Start(); //启动进程
 				Console.WriteLine(processTP.StandardOutput.ReadToEnd());
 				processTP.WaitForExit();
@@ -79,7 +89,8 @@ namespace TextureBatchPacker
 
 			try
 			{
-				string[] newArgs = { "-i", "C:\\", "-o", "D:\\" };
+				//string[] newArgs = { "-t", "True", "-i", "C:\\", "-o", "D:\\" };
+				string[] newArgs = { "--help" };
 				Args.InvokeMain<TextureBatchPackerArgs>(newArgs);
 			}
 			catch (Exception ex)
