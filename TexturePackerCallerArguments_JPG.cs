93bdc34eefa76adf583eac80d5ee996d08307039
张鑫 <xin.zhang@catmintgame.com>
2019-08-15 18:57:45 +0800

    TP默认最大纹理大小为2048，需要显式指定最大大小。

--- TexturePackerCallerArguments_JPG.cs
+++ TexturePackerCallerArguments_JPG.cs
@@ -30,7 +30,7 @@ namespace TextureBatchPacker
 			if (parameters.NoTrim)
 			{
 				argument = string.Format(
-					"--format cocos2d-v2 --data \"{0}\" {1}--texture-format jpg --jpg-quality {2} --dpi 72 --opt RGB888 --size-constraints POT --force-squared --scale {3} --scale-mode Smooth --algorithm MaxRects --maxrects-heuristics Best --pack-mode Best --border-padding 0 --shape-padding 0 --inner-padding 0 --extrude 1 --disable-rotation --trim-mode None \"{4}\"",
+					"--format cocos2d-v2 --data \"{0}\" {1}--texture-format jpg --jpg-quality {2} --dpi 72 --opt RGB888 --max-size 4096 --size-constraints POT --force-squared --scale {3} --scale-mode Smooth --algorithm MaxRects --maxrects-heuristics Best --pack-mode Best --border-padding 0 --shape-padding 0 --inner-padding 0 --extrude 1 --disable-rotation --trim-mode None \"{4}\"",
 					getPlistFullPath(parameters),
 					GetTrimSpriteNamesArgument(),
 					100,
@@ -40,7 +40,7 @@ namespace TextureBatchPacker
 			else
 			{
 				argument = string.Format(
-					"--format cocos2d-v2 --data \"{0}\" {1}--texture-format jpg --jpg-quality {2} --dpi 72 --opt RGB888 --size-constraints POT --force-squared --scale {3} --scale-mode Smooth --algorithm MaxRects --maxrects-heuristics Best --pack-mode Best --border-padding 0 --shape-padding 0 --inner-padding 0 --extrude 1 --enable-rotation --trim-mode Trim --trim-threshold 2 \"{4}\"",
+					"--format cocos2d-v2 --data \"{0}\" {1}--texture-format jpg --jpg-quality {2} --dpi 72 --opt RGB888 --max-size 4096 --size-constraints POT --force-squared --scale {3} --scale-mode Smooth --algorithm MaxRects --maxrects-heuristics Best --pack-mode Best --border-padding 0 --shape-padding 0 --inner-padding 0 --extrude 1 --enable-rotation --trim-mode Trim --trim-threshold 2 \"{4}\"",
 					getPlistFullPath(parameters),
 					GetTrimSpriteNamesArgument(),
 					100,
